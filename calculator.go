187b40efdf67dbc9db28be5a65daeb9bac7f1d30
Christopher Eyre <chriseyre2000@gmail.com>
2022-03-02 05:26:20 +0000

    Added basic Multiply

---
+++ calculator.go
@@ -0,0 +1,14 @@
+package calculator
+
+// Takes a + b and returns the sum
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Subtract(a, b float64) float64 {
+	return a - b
+}
