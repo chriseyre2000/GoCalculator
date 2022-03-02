40c0e53753a0357487a892504840625122a45280
Christopher Eyre <christopher.eyre@codurance.com>
2022-03-02 07:50:07 +0000

    Implemented and tested a Sqrt function

--- calculator.go
+++ calculator.go
@@ -1,6 +1,9 @@
 package calculator
 
-import "errors"
+import (
+	"errors"
+	maths "math"
+)
 
 // Takes a + b and returns the sum
 func Add(a, b float64) float64 {
@@ -16,6 +19,13 @@ func Divide(a, b float64) (float64, error) {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, errors.New("square root of negative numbers is not allowed")
+	}
+	return maths.Sqrt(a), nil
 }
 
 func Subtract(a, b float64) float64 {
--- calculator_test.go
+++ calculator_test.go
@@ -37,10 +37,10 @@ func TestDivide(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		{a: 2, b: 2, want: 1,},
-		{a: 7, b: 2, want: 3.5,},
-		{a: 1, b: -1, want: -1,},
-		{a: 1, b: 3, want: 0.333333,},
+		{a: 2, b: 2, want: 1},
+		{a: 7, b: 2, want: 3.5},
+		{a: 1, b: -1, want: -1},
+		{a: 1, b: 3, want: 0.333333},
 	}
 
 	for _, tc := range testCases {
@@ -75,18 +75,18 @@ func TestDivideInvalidInput(t *testing.T) {
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	
+
 	type testCase struct {
 		a, b float64
 		want float64
 	}
-	
+
 	testCases := []testCase{
 		{a: 2, b: 3, want: 6},
 		{a: 6, b: 0.5, want: 3},
 		{a: -5, b: -5, want: 25},
 	}
-	
+
 	for _, tc := range testCases {
 		got := calculator.Multiply(tc.a, tc.b)
 		if tc.want != got {
@@ -117,6 +117,49 @@ func TestSubtract(t *testing.T) {
 	}
 }
 
+func TestSquareRoot(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a    float64
+		want float64
+	}
+
+	testCases := []testCase{
+		{a: 9, want: 3},
+		{a: 0.01, want: 0.1},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.a)
+		if err != nil {
+			t.Fatalf("Sqrt(%f): want no error for valid input got, %v", tc.a, err)
+		}
+		if !closeEnough(tc.want, got, 0.1) {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+}
+
+func TestSqrtInvalidInput(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a float64
+	}
+
+	testCases := []testCase{
+		{a: -1,},
+	}
+
+	for _, tc := range testCases {
+		_, err := calculator.Sqrt(tc.a)
+		if err == nil {
+			t.Fatalf("Sqrt(%f): want error for invalid input got nil", tc.a)
+		}
+	}
+}
+
 func closeEnough(a, b, tolerance float64) bool {
-	return maths.Abs(a - b) <= tolerance
+	return maths.Abs(a-b) <= tolerance
 }
